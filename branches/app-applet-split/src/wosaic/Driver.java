74bdb39487d81cb932b6a107f39f9aaf08a698b4
swegner2 <swegner2@711047cf-0c3e-0410-8c02-7d3084e1682f>
2008-01-26 18:19:53 +0000

    Second try at this branch...

---
+++ branches/app-applet-split/src/wosaic/Driver.java
@@ -0,0 +1,16 @@
+package wosaic;
+
+//package tests;
+import javax.media.jai.*;
+import wosaic.utilities.Pixel;
+import wosaic.utilities.Facebook;
+
+public class Driver {
+	public static void main(String args[]) {
+
+		// Initialize the application version of the UI
+		WosaicApp app = new WosaicApp();
+		app.setVisible(true);
+		
+	}
+}
