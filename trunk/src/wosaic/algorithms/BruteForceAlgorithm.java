4470e9068f459f4ea2d480c00b7787a6d8c243df
swegner2 <swegner2@711047cf-0c3e-0410-8c02-7d3084e1682f>
2008-09-17 19:50:21 +0000

    More work on the BruteForceAlgorithm-- I think it's ready to encorporate

--- trunk/src/wosaic/algorithms/BruteForceAlgorithm.java
+++ trunk/src/wosaic/algorithms/BruteForceAlgorithm.java
@@ -3,6 +3,11 @@
  */
 package wosaic.algorithms;
 
+import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import wosaic.utilities.Mosaic;
 import wosaic.utilities.Pixel;
 
@@ -16,6 +21,11 @@ import wosaic.utilities.Pixel;
  */
 public class BruteForceAlgorithm extends AbstractAlgorithm {
 
+	protected LinkedBlockingQueue<Pixel> PixelQueue;
+	protected AtomicBoolean running;
+	protected AtomicBoolean pluginFinished;
+	private long TIMEOUT = 50;
+	
 	/**
 	 * Default constructor-- simple call our superclass constructor
 	 * 
@@ -24,6 +34,9 @@ public class BruteForceAlgorithm extends AbstractAlgorithm {
 	 */
 	public BruteForceAlgorithm(Mosaic mos, int[][][] colorMap) {
 		super(mos, colorMap);
+		PixelQueue = new LinkedBlockingQueue<Pixel>();
+		running = new AtomicBoolean(false);
+		pluginFinished = new AtomicBoolean(false);
 	}
 
 	/**
@@ -35,6 +48,19 @@ public class BruteForceAlgorithm extends AbstractAlgorithm {
 	 */
 	@Override
 	public void AddPixel(Pixel pixel) {
+		// Start running if we're not
+		boolean wasRunning = running.getAndSet(true);
+		if (!wasRunning) {
+			Thread workThread = new Thread(this, "AlgorithmThread");
+			workThread.run();
+		}
+		
+		PixelQueue.add(pixel);
+		PixelQueue.notifyAll();
+	}
+	
+	private void processPixel(Pixel pixel) {
+
 		final int[] avgColors = new int[3];
 		for (int r = 0; r < Mos.getParams().resRows; r++)
 			for (int c = 0; c < Mos.getParams().resCols; c++) {
@@ -49,9 +75,59 @@ public class BruteForceAlgorithm extends AbstractAlgorithm {
 				else if (matchScore < Mos.getScoreAt(r, c))
 					Mos.UpdatePixel(r, c, pixel, matchScore);
 			}
-
-		// FIXME: What does this do?
-		notifyAll();
+	}
+	
+	/**
+	 * Consider a list of new Pixels that have been generated from the source plugin.
+	 * The default behavior is to simply process them one-by-one in AddPixel().  However,
+	 * new Algorithms are encouraged to override this as appropriate
+	 * 
+	 * @param pixels The new Pixel objects to consider
+	 */
+	@Override
+	public void AddPixels(ArrayList<Pixel> pixels) {
+		// Start running if we're not
+		boolean wasRunning = running.getAndSet(true);
+		if (!wasRunning) {
+			Thread workThread = new Thread(this, "AlgorithmThread");
+			workThread.run();
+		}
+		
+		PixelQueue.addAll(pixels);
+		PixelQueue.notifyAll();
 	}
 
+	@Override
+	public void run() {
+		boolean waitForMore = true;
+		boolean finished = false;
+		
+		while(!finished) {
+			if (waitForMore)
+				waitForMore = !pluginFinished.get();
+			
+			Pixel pixel = null;
+			try {
+				pixel = PixelQueue.poll(TIMEOUT, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException ex) {
+				// We've been canceled!  Cleanup?
+				waitForMore = false;
+			}
+			
+			if (pixel == null) {
+				if (waitForMore)
+					// Simply try again
+					continue;
+				else
+					finished = true;
+				
+			} else // pixel != null
+				processPixel(pixel);
+		}
+	}
+
+	@Override
+	public void FinishedAddingPixels() {
+		pluginFinished.set(true);
+	}
 }
